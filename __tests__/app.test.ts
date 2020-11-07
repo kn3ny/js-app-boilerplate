70200a0cef4375a61d99d5b096bddcb3f964d1b7
kn3ny <30493783+kn3ny@users.noreply.github.com>
2020-11-07 17:30:01 +0900

    fix test

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -2,6 +2,6 @@ import { App } from "../src/app";
 
 describe("App", () => {
   test("greeting()", () => {
-    expect(new App().greeting()).toStrictEqual("Hello world");
+    expect(new App().greeting()).toStrictEqual("Hello world!");
   });
 });
